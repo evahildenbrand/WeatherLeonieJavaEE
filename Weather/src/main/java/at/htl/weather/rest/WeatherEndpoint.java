adbdd814f2875f9b0f060d3ed0c55c82d803eca0
evahildenbrand <1eva.hildenbrand@gmail.com>
2019-10-24 11:51:02 +0200

    Added method actualWeather

--- Weather/src/main/java/at/htl/weather/rest/WeatherEndpoint.java
+++ Weather/src/main/java/at/htl/weather/rest/WeatherEndpoint.java
@@ -1,4 +1,14 @@
 package at.htl.weather.rest;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import java.util.List;
+
+@Path("/")
 public class WeatherEndpoint {
+
+    @GET
+    public String actualWeather(){
+        return "hello";
+    }
 }
