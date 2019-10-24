ba011e05c6cab8eff3b7f93529c8308e0712cd8d
evahildenbrand <1eva.hildenbrand@gmail.com>
2019-10-24 12:06:38 +0200

    Added variables for CSV

--- Weather/src/main/java/at/htl/weather/rest/WeatherEndpoint.java
+++ Weather/src/main/java/at/htl/weather/rest/WeatherEndpoint.java
@@ -2,10 +2,20 @@ package at.htl.weather.rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import java.io.BufferedReader;
 import java.util.List;
 
 @Path("/")
 public class WeatherEndpoint {
+    String csvFile = "http://www.zamg.ac.at/ogd/";
+    BufferedReader br = null;
+    String splitCsv = "/\r?\n|\r/";
+    int lineIndex = 1;
+    String splitLine = ";";
+    int tempIndex = 5;
+    int windSpeedIndex = 9;
+    int rainIndex = 12;
+    int sunIndex = 15;
 
     @GET
     public String actualWeather(){
