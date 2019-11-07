784174dbfd3d380f213a57dc69ce768f3b435f4a
evahildenbrand <1eva.hildenbrand@gmail.com>
2019-11-07 12:37:42 +0100

    Deleted JavaEE-Programm

--- Weather/src/main/java/at/htl/weather/rest/RestConfig.java
+++
@@ -1,8 +0,0 @@
-package at.htl.weather.rest;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("api")
-public class RestConfig extends Application {
-}
