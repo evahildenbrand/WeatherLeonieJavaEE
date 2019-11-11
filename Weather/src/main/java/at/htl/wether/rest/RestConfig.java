78f6d5aac5206e74e8cc99ac958882d2cfdfed68
evahildenbrand <1eva.hildenbrand@gmail.com>
2019-11-11 08:56:33 +0100

    Added ApplicationPath and extends

--- Weather/src/main/java/at/htl/wether/rest/RestConfig.java
+++ Weather/src/main/java/at/htl/wether/rest/RestConfig.java
@@ -1,4 +1,8 @@
 package at.htl.wether.rest;
 
-public class RestConfig {
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class RestConfig extends Application {
 }
